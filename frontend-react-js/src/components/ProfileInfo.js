67ca7f27caeda958cb2e6f7dccde84a2ea428660
Madison Jordan <madison.jordan.work@gmail.com>
2023-03-10 18:06:16 +0000

    update user pool

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -2,7 +2,6 @@ import './ProfileInfo.css';
 import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
 import React from "react";
 
-// [TODO] Authenication
 import { Auth } from 'aws-amplify';
 
 export default function ProfileInfo(props) {
