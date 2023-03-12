b70deb136c767496d5ad303d186d0250d4ef0b3b
Madison Jordan <madison.jordan.work@gmail.com>
2023-03-12 22:12:05 +0000

    add cognito jwt authentication

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,7 +4,7 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run():
+  def run(cognito_user_id=None):
     # CLOUDWATCH -----
     #logger.info('Hello Cloudwatch! from  /api/activities/home')
     # HONEYCOMB ------
@@ -51,5 +51,17 @@ class HomeActivities:
         'replies': []
       }
       ]
+      if cognito_user_id != None:
+        extra_crud = {
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Lore',
+          'message': 'My dear brother, it the humans that are the problem',
+          'created_at': (now - timedelta(hours=1)).isoformat(),
+          'expires_at': (now + timedelta(hours=12)).isoformat(),
+          'likes': 1042,
+          'replies': []
+        }
+        results.insert(0, extra_crud)
+
       span.set_attribute("app.result_length", len(results))
       return results
