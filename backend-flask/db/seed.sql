1a86fe09a111ca5eeebaa104bfdb1d6b9b65ba4c
Madison Jordan <madison.jordan.work@gmail.com>
2023-04-07 04:22:05 +0000

    add script to update users with cognito id

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,10 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' , 'andrew@example.com', 'MOCK'),
-  ('Andrew Bayko', 'bayko' , 'bayko@example.com', 'MOCK');
+  ('Andrew Brown', 'andrewbrown', 'andrew@example.com', 'MOCK'),
+  ('Andrew Bayko', 'bayko', 'bayko@example.com', 'MOCK'),
+  ('Madison Jordan', 'madisonjordan', 'mjordan@example.com', 'MOCK'),
+  ('Julia Sugarbaker', 'juliasugarbaker', 'jsugarbaker@example.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
