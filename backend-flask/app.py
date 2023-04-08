fbcaec29a3d9c6e26d9d9e5ba02ada0f59c25644
Madison Jordan <madison.jordan.work@gmail.com>
2023-04-08 19:21:26 +0000

    change message route to use message group uuid

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -148,8 +148,8 @@ def data_message_groups():
     app.logger.debug(e)
     return {}, 401
 
-@app.route("/api/messages/@<string:handle>", methods=['GET'])
-def data_messages(handle):
+@app.route("/api/messages/<string:message_group_uuid>", methods=['GET'])
+def data_messages(message_group_uuid):
   access_token = extract_access_token(request.headers)
   try:
     claims = cognito_jwt_token.verify(access_token)
