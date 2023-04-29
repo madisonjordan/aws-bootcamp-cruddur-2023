41c94a2c984b9017078a167876630b5f101c8a15
Madison Jordan <madison.jordan.work@gmail.com>
2023-04-29 22:52:08 +0000

    update docker build utility scripts

--- bin/docker/build/backend-flask-dev.sh
+++ bin/docker/build/backend-flask-dev.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
+#! /usr/bin/bash
 
-docker build -t backend-flask:dev -f Dockerfile .
+SCRIPT_PATH=$(dirname $(readlink -f "$0"))
+# bin/docker/build
+BIN_PATH=$(dirname $(dirname $SCRIPT_PATH))
+PROJECT_PATH=$(dirname $BIN_PATH)
+BACKEND_FLASK_PATH="$PROJECT_PATH/backend-flask"
+
+docker build \
+-f "$BACKEND_FLASK_PATH/Dockerfile" \
+-t backend-flask:dev \
+"$BACKEND_FLASK_PATH"
--- bin/docker/build/backend-flask-prod.sh
+++ bin/docker/build/backend-flask-prod.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
+#! /usr/bin/bash
 
-docker build -t backend-flask -f Dockerfile.prod .
+SCRIPT_PATH=$(dirname $(readlink -f "$0"))
+# bin/docker/build
+BIN_PATH=$(dirname $(dirname $SCRIPT_PATH))
+PROJECT_PATH=$(dirname $BIN_PATH)
+BACKEND_FLASK_PATH="$PROJECT_PATH/backend-flask"
+
+docker build \
+-f "$BACKEND_FLASK_PATH/Dockerfile.prod" \
+-t backend-flask \
+"$BACKEND_FLASK_PATH"
