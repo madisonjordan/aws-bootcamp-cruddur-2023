1f8b711e4db2edb9db1a9fbbfe6710c917b1ff7a
Madison Jordan <madison.jordan.work@gmail.com>
2023-05-27 19:52:36 +0000

    fix displayed post creation and expiration time

--- frontend-react-js/src/lib/DateTimeFormats.js
+++ frontend-react-js/src/lib/DateTimeFormats.js
@@ -31,15 +31,15 @@ export function time_ago(value){
   const datetime = DateTime.fromISO(value, { zone: 'utc' })
   const future = datetime.setZone(Intl.DateTimeFormat().resolvedOptions().timeZone);
   const now     = DateTime.now()
-  const diff_mins = future.diff(now, 'minutes').toObject().minutes;
-  const diff_hours = future.diff(now, 'hours').toObject().hours;
-  const diff_days = future.diff(now, 'days').toObject().days;
+  const diff_mins = now.diff(future, 'minutes').toObject().minutes;
+  const diff_hours = now.diff(future, 'hours').toObject().hours;
+  const diff_days = now.diff(future, 'days').toObject().days;
 
-  if (diff_hours > 24.0){
-    return `${Math.floor(diff_days)}d`;
+  if (Math.abs(diff_hours) > 24.0){
+    return `${Math.floor(Math.abs(diff_days))}d`;
   } else if (diff_hours < 24.0 && diff_hours > 1.0) {
-    return `${Math.floor(diff_hours)}h`;
+    return `${Math.floor(Math.abs(diff_hours))}h`;
   } else if (diff_hours < 1.0) {
-    return `${Math.round(diff_mins)}m`;
+    return `${Math.round(Math.abs(diff_mins))}m`;
   }
 }
