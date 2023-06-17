4e7c55fd837bbac5eb088014724e2974d81a7420
Madison Jordan <madison.jordan.work@gmail.com>
2023-06-17 20:02:55 +0000

    update activity feed component

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -6,9 +6,11 @@ import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
+import EditProfileButton from '../components/EditProfileButton';
 
 // [TODO] Authenication
 import Cookies from 'js-cookie'
+import { prependOnceListener } from 'process';
 
 export default function UserFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -63,7 +65,12 @@ export default function UserFeedPage() {
       <DesktopNavigation user={user} active={'profile'} setPopped={setPopped} />
       <div className='content'>
         <ActivityForm popped={popped} setActivities={setActivities} />
-        <ActivityFeed title={title} activities={activities} />
+        <div className='activity_feed'>
+          <div className='activity_feed_heading'>
+            <div className='title'>{title}</div>
+          </div>
+          <ActivityFeed title={title} activities={activities} />
+        </div>
       </div>
       <DesktopSidebar user={user} />
     </article>
