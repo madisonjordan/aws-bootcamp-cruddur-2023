f21c785d2e95077e22648aec743bc72341e6893a
Madison Jordan <madison.jordan.work@gmail.com>
2023-09-18 01:43:57 +0000

    fix activities authentication

--- frontend-react-js/src/components/ActivityForm.js
+++ frontend-react-js/src/components/ActivityForm.js
@@ -2,6 +2,7 @@ import './ActivityForm.css';
 import React from "react";
 import process from 'process';
 import {ReactComponent as BombIcon} from './svg/bomb.svg';
+import { getAccessToken } from '../lib/CheckAuth';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -19,12 +20,13 @@ export default function ActivityForm(props) {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities`
       console.log('onsubmit payload', message)
+      const access_token = await getAccessToken();
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({
           user_handle: props.user_handle.handle,
