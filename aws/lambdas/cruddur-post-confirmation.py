92c224c6c376ec59f24b58707839f5fa0abf8993
Madison Jordan <madison.jordan.work@gmail.com>
2023-09-18 01:43:24 +0000

    fix frontend signup

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -9,22 +9,35 @@ def lambda_handler(event, context):
     user_display_name  = user['name']
     user_email         = user['email']
     user_handle        = user['preferred_username']
-    user_cognito_id    = user['sub']
+    cognito_user_id    = user['sub']
     try:
-      sql = f"INSERT INTO public.users (display_name, email, handle, cognito_user_id) VALUES(%s,%s,%s,%s)"
-
+      print('entered-try')
+      sql = f"""
+         INSERT INTO public.users (
+          display_name, 
+          email,
+        VALUES(
+          %(display_name)s,
+          %(email)s,
+          %(handle)s,
+          %(cognito_user_id)s
+        )
+      """
+      print('SQL Statement ----')
+      print(sql)
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
-      params = [
-        user_display_name,
-        user_email,
-        user_handle,
-        user_cognito_id
-      ]
-      cur.execute(sql, params)
+      params = {
+        'display_name': user_display_name,
+        'email': user_email,
+        'handle': user_handle,
+        'cognito_user_id': cognito_user_id
+      }
+      cur.execute(sql,params)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
+      print('error:')
       print(error)
     finally:
       if conn is not None:
--- journal/weekX.md
+++ journal/weekX.md
@@ -1,1 +1,18 @@
 # Week X — Clean Up Week
+
+
+### Cruddur Database Setup (RDS)
+RDS Connection:
+- add new inbound connection for gitpod ip in the RDS Security Group created by CFN
+- update `DB_SG_ID` and `DB_SG_RULE_ID` env vars for the `update-sg-rule` script
+- test connect to prod
+  
+Creating Cruddur Tables:
+- load schema `./bin/db/schema-load prod`
+- add bio column from migration `CONNECTION_URL=$PROD_CONNECTION_URL ./bin/db/migrate`
+
+Fix Lambda-RDS Connection:
+- update connection_url var in lambda
+- add SG for Cognito Lambda in the cfn cluster network vpc
+  - doesn't need inbound rules
+- add inbound security group rule in the RDS SG that uses the newly created Cognito-Lambda SG for cognito post signup confirmation 
