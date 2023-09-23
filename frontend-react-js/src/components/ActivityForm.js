d0f8966258ce05402d50d1ff8297835b400dd9e3
Madison Jordan <madison.jordan.work@gmail.com>
2023-09-23 23:51:19 +0000

    fix hardcoded user in create activities

--- frontend-react-js/src/components/ActivityForm.js
+++ frontend-react-js/src/components/ActivityForm.js
@@ -2,7 +2,7 @@ import './ActivityForm.css';
 import React from "react";
 import process from 'process';
 import {ReactComponent as BombIcon} from './svg/bomb.svg';
-import { getAccessToken } from '../lib/CheckAuth';
+import {getAccessToken} from '../lib/CheckAuth';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -20,16 +20,15 @@ export default function ActivityForm(props) {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities`
       console.log('onsubmit payload', message)
-      const access_token = await getAccessToken();
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
           'Authorization': `Bearer ${access_token}`,
-          'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          user_handle: props.user_handle.handle,
           message: message,
           ttl: ttl
         }),
