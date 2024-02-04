d5ab597c632b427a1f561db18f19ba73f3efb8d1
Madison Jordan <madijrdn@gmail.com>
2024-02-04 13:44:11 -0800

    update jwt error handling

--- backend-flask/lib/cognito_jwt_token.py
+++ backend-flask/lib/cognito_jwt_token.py
@@ -139,7 +139,7 @@ def jwt_required(f=None, on_error=None):
             # unauthenticated request
             app.logger.debug(e)
             if on_error:
-                on_error(e)
+                return on_error(e)
             return {}, 401
         return f(*args, **kwargs)
     return decorated_function
